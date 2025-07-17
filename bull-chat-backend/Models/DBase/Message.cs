acc39375f98cae4434d3f28a89817c40e9c59d0a
Pavel Dudin <pashalol3@mail.ru>
2025-07-17 09:11:31 +0300

    [NEW]	Добавлен конструктор

--- bull-chat-backend/Models/DBase/Message.cs
+++ bull-chat-backend/Models/DBase/Message.cs
@@ -4,12 +4,22 @@
     public class Message
     {
         public int Id { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
         public int UserId { get; set; }
         public int ContentId { get; set; }
         public User? User { get; set; }
         public Content? Content { get; set; }
 
+        public Message(User msgFrom, string text)
+        {
+            User = msgFrom;
+            UserId = msgFrom.Id;
+            Content = new Content()
+            {
+                Text = text
+            };
+        }
+        public Message() { }
         public static Message Empty => new()
         {
             Id = int.MinValue,
