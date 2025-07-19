f35a261213786841abd5d412caa4b5a23b9ab3ac
plrtp <plrtp@mail.ru>
2025-07-19 17:01:17 +0300

    update: authController возращает токен без Bearer, так как для signalr настройки Bearer не нужен

--- bull-chat-backend/Controllers/AuthenticationController.cs
+++ bull-chat-backend/Controllers/AuthenticationController.cs
@@ -46,7 +46,7 @@ namespace bull_chat_backend.Controllers
 
             return Ok(new
             {
-                Token = $"Bearer {jwtToken}"
+                Token = $"{jwtToken}"
             });
         }
 
