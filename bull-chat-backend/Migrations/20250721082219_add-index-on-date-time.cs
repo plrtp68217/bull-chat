cd0b06db53e825dbd11b3b37e56fe31cae618b66
Pavel Dudin <pashalol3@mail.ru>
2025-07-21 12:08:51 +0300

    [UPD]	Добавлен индекс на DateTime

---
+++ bull-chat-backend/Migrations/20250721082219_add-index-on-date-time.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bull_chat_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class addindexondatetime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Message_Date",
+                table: "Message",
+                column: "Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Message_Date",
+                table: "Message");
+        }
+    }
+}
