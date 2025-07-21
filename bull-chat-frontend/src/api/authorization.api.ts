df26b5bbcaf984ee71362fc80d742750528da941
plrtp <plrtp@mail.ru>
2025-07-21 17:32:42 +0300

    add: в api добавлены методы проверки токена на валидность и выхода из аккаунта

--- bull-chat-frontend/src/api/authorization.api.ts
+++ bull-chat-frontend/src/api/authorization.api.ts
@@ -7,8 +7,16 @@ export default {
     const response = await apiClient.post('/authentication/login', dto);
     return response.data;
   },
+  async logout(clientHash: string): Promise<string> {
+    const response = await apiClient.post('/authentication/logout', clientHash);
+    return response.data;
+  },
   async register(dto: IAuthDto): Promise<string> {
     const response = await apiClient.post('/authentication/register', dto);
     return response.data;
   },
+  async validateJwt(token: string): Promise<Boolean>  {
+    const response = await apiClient.post('/authentication/validate-jwt', token);
+    return response.data;
+  }
 }
