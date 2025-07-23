307f84dac8896f8b31357ff7f4c2f677861c26fe
Pavel Dudin <pashalol3@mail.ru>
2025-07-23 16:27:05 +0300

    modified:   src/api/messages.api.ts

--- bull-chat-frontend/src/api/messages.api.ts
+++ bull-chat-frontend/src/api/messages.api.ts
@@ -3,7 +3,7 @@ import type { IMessagesResponse } from "./interfaces/messages/IMessagesResponse"
 
 export default {
   async getMessages(messageId: number | null): Promise<IMessagesResponse[]>  {
-    const response = await apiClient.post('/message/next-message-page', messageId);
+    const response = await apiClient.post('/message/next-message-page-index', messageId);
     return response.data;
   },
 }
