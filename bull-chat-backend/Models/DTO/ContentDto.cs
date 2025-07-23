33fd4daa973d876256f67402a9d920ae4da13d89
plrtp <plrtp@mail.ru>
2025-07-23 10:18:15 +0300

    update: поле contenttype content изменено на contenttype contenttype

--- bull-chat-backend/Models/DTO/ContentDto.cs
+++ bull-chat-backend/Models/DTO/ContentDto.cs
@@ -5,6 +5,6 @@ namespace bull_chat_backend.Models.DTO
     public class ContentDto(string item, ContentType content)
     {
         public string Item { get; } = item;
-        public ContentType Content { get; } = content;
+        public ContentType ContentType { get; } = content;
     }
 }
