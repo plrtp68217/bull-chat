78dc38a3431fa9937a8c8aceed28d12298f647ed
Pavel Dudin <pashalol3@mail.ru>
2025-07-23 16:57:09 +0300

    new file:   bull-chat-backend/.config/dotnet-tools.json 	new file:   docker-compose.yml

--- bull-chat-frontend/src/api/apiClient.ts
+++ bull-chat-frontend/src/api/apiClient.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
+const baseURL = `${import.meta.env.VITE_API_URL.replace(/\/+$/, '')}/api`;
 
 const apiClient = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: 'http://localhost:5081/api/',
+  baseURL: baseURL,
+  // baseURL: 'http://localhost:5081/api/',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
