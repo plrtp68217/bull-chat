fad5e9063135f44ecf59b06baf49ad9c4e51abce
Pavel Dudin <pashalol3@mail.ru>
2025-07-23 16:58:26 +0300

    Revert "	new file:   bull-chat-backend/.config/dotnet-tools.json"
    This reverts commit 78dc38a3431fa9937a8c8aceed28d12298f647ed.

--- bull-chat-frontend/src/api/apiClient.ts
+++ bull-chat-frontend/src/api/apiClient.ts
@@ -1,9 +1,8 @@
 import axios from 'axios'
-const baseURL = `${import.meta.env.VITE_API_URL.replace(/\/+$/, '')}/api`;
 
 const apiClient = axios.create({
-  baseURL: baseURL,
-  // baseURL: 'http://localhost:5081/api/',
+  // baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'http://localhost:5081/api/',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
